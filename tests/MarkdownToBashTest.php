7f17e2192ee1e57dcc35a820f47124b31401bfc2
ATCO Dias <diaslasd@gmail.com>
2024-10-21 11:42:02 -0300

    🚨 Adding a test

--- tests/MarkdownToBashTest.php
+++ tests/MarkdownToBashTest.php
@@ -1,23 +1,62 @@
 <?php
 
-   namespace Diaslasd\Tests;
+namespace Diaslasd;
 
-   use Diaslasd\MarkdownToBash;
-   use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 
-   class MarkdownToBashTest extends TestCase
-   {
-       public function testConvert()
-       {
-           $markdown = "# Título\n\nUm texto qualquer.\n\n- Item 1\n- Item 2";
-           $markdownToBash = new MarkdownToBash();
-           $output = $markdownToBash->convert($markdown);
+class MarkdownToBashTest extends TestCase
+{
+    private MarkdownToBash $markdownToBash;
 
-           $expectedOutput = "» Título\n\nUm texto qualquer.\n\n" .
-               "\033[33m- Item 1\033[0m\n" .
-               "\033[33m- Item 2\033[0m";
+    protected function setUp(): void
+    {
+        $this->markdownToBash = new MarkdownToBash();
+    }
 
-           $this->assertEquals($expectedOutput, $output);
-       }
-   }
-   
+    public function testFormatMarkdownString(): void
+    {
+        $markdown = "# Título 1\n\n## Título 2\n\n- Item 1\n- Item 2\n\n**Negrito**\n\n*Itálico*\n\n[Link](https://www.example.com)\n\n| Coluna 1 | Coluna 2 |\n|---|---|";
+        $expected = '» [46m[1mTítulo 1[0m
+
+»» [46m[1mTítulo 2[0m
+[33m
+- Item 1[0m
+[33m- Item 2[0m
+
+[1mNegrito[0m
+
+[4mItálico[0m
+
+[4mLink[0m ([34mhttps://www.example.com[0m)
+
+[36mColuna 1       [0m[36mColuna 2       [0m
+[36m---            [0m[36m---            [0m'; 
+        $this->assertEquals($expected, $this->markdownToBash->convert($markdown));
+    }
+
+    public function testConvertWithCodeBlock(): void
+    {
+        $markdown = "# Título\n\n
+php\n<?php\necho 'Olá, mundo!';\n
+👾 ";
+      $expected = "» [46m[1mTítulo[0m
+
+php
+<?php
+echo 'Olá, mundo!';
+
+👾 "; 
+        $this->assertEquals($expected, $this->markdownToBash->convert($markdown));
+    }
+
+    public function testConvertWithoutCodeBlock(): void
+    {
+        $markdown = "# Título\n\n- Item 1\n- Item 2";
+        $expected = '» [46m[1mTítulo[0m
+[33m
+- Item 1[0m
+[33m- Item 2[0m';
+        $this->assertEquals($expected, $this->markdownToBash->convert($markdown));
+    }
+}
+
